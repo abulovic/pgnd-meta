4b488c8d0842bb25c719fcd93ee0ae46978b5680
Ana Bulovic <bulovic.ana@gmail.com>
2016-04-28 18:41:03 +0200

    Add support for connecting to NCBI database

--- meta/util.py
+++ meta/util.py
@@ -2,7 +2,10 @@ import os
 import sys
 import time
 import math
+import sqlite3
 from contextlib import contextmanager
+
+import meta
 
 @contextmanager
 def timeit_msg(msg):
@@ -29,5 +32,11 @@ def get_file_size(fname):
 	else:
 		res = 'GB'
 	return '{:1.2f} {}'.format(float(size) / 10**divisor[res], res)
+
+def get_ncbi_db_conn():
+	dbfile = os.path.join(meta.__path__[0], 'data', 'NCBI.db')
+	conn = sqlite3.connect(dbfile)
+	conn.text_factory = str
+	return conn
 
 
